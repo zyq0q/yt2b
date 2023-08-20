529026e192cd67ee26936f0560cd8e8ceb785c98
zyq0q <stayforlove>
2023-08-20 23:26:26 +0800

    commit

--- replace_bgm.sh
+++ replace_bgm.sh
@@ -5,13 +5,22 @@ Dir="video_file/file_"${Date}/
 echo $Dir
 mkdir -p $Dir
 cd $Dir
-video_name=`ls -t | grep mp4  |head -n1|awk '{print $0}'`
-echo $video_name
-info_json_name=${video_name/%.mp4/.info.json}
-echo $info_json_name
 rm -f a.mp4
-ffmpeg -y -i "$video_name" -i /opt/bgm/DeadEyes.mp3 -c:v copy -c:a aac -strict experimental -map 0:v:0 -map 1:a:0 "a.mp4"
+video_name=`ls -t *.mp4  |head -n1|awk '{print $0}'`
+
+echo $video_name
+info_json_name=${video_name/%.mp4/.mp4.info.json}
+echo $info_json_name
+
+#ffmpeg -y -i "$video_name" -i /opt/bgm/DeadEyes.mp3 -c:v copy -c:a aac -strict experimental -map 0:v:0 -map 1:a:0 "a.mp4"
+
+#ffmpeg -y -i "$video_name" -i /opt/bgm/DeadEyes.mp3 -vcodec copy -filter_complex amix -map 0:v -map 0:a -map 1:a -shortest  a.mp4
+
+ffmpeg -y -i "$video_name" -i /opt/bgm/DeadEyes.mp3 -c:v copy -c:a aac -strict experimental -map 0:v:0 -map 1:a:0 -shortest "a.mp4"
+
+
 \cp "$info_json_name" ./a.info.json
 
-cover_name=${video_name/%.mp4/.webp}
+cover_name=${video_name/%.mp4/.mp4.webp}
+
 \cp "$cover_name" ./a.webp
