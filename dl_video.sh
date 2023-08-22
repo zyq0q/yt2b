0867f306fbcc4d082f5bd3dca23eba5601addf3e
zyq0q <stayforlove>
2023-08-22 22:48:32 +0800

    commit

--- dl_video.sh
+++ dl_video.sh
@@ -8,5 +8,5 @@ cd $Dir
 ind_date=`date -d "1 day ago" +"%Y%m%d"`
 
 
-yt-dlp  --playlist-end 1 --write-thumbnail --write-thumbnail --write-info -f b*  -o '%(id)s.mp4'  --merge-output-format mp4 https://www.youtube.com/watch?v=Xv62yzQ4fM8
+yt-dlp  --playlist-end 1 --write-thumbnail  --convert-thumbnails jpg --write-info -f b*  -o '%(id)s.mp4'  --merge-output-format mp4 https://www.youtube.com/watch?v=Xv62yzQ4fM8
 
